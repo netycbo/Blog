823bd6dbd2ee9f5f76b06b404b2c5a4ee3659520
Netycbo <mikolaj.silinski@gmail.com>
2024-03-13 13:34:17 +0100

    folder for entities and migration

---
+++ Blog.DataAccess/BlogStorageontextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Blog.DataAccess
+{
+    public class BlogStorageontextFactory : IDesignTimeDbContextFactory<BlogstorageContext>
+    {
+        public BlogstorageContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BlogstorageContext>();
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-5N54V80V\\SQLEXPRESS;Initial Catalog=BlogStorage;Integrated Security=True;Trust Server Certificate=True");
+            return new BlogstorageContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Blog.DataAccess/BlogstorageContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Blog.DataAccess.Entities;
+
+namespace Blog.DataAccess
+{
+    public class BlogstorageContext : DbContext
+    {
+        public BlogstorageContext(DbContextOptions<BlogstorageContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
